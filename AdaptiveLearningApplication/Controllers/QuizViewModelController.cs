9e8fc2f29250860d3ea6a307e4b59a5374cb46b0
afatir <fatir@lycus.com.pk>
2016-07-05 18:06:54 +0500

    Final Commit

---
+++ AdaptiveLearningApplication/Controllers/QuizViewModelController.cs
@@ -0,0 +1,39 @@
+﻿using AdaptiveLearningApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdaptiveLearningApplication.Controllers
+{
+    public class QuizViewModelController : Controller
+    {
+        private AdaptiveLearningContext db = new AdaptiveLearningContext();
+
+        //
+        // GET: /QuizViewModel/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult TakeQuiz(int id = 0)
+        {
+            var quizviewmodel = new QuizViewModel();
+            quizviewmodel.Quiz = db.Quiz.Find(id);
+            quizviewmodel.Quiz.Questions = db.QuestionPool.Where(m => m.DifficultyLevel == 1).ToList(); //.Where(m => m.DifficultyLevel == 1)
+            //quizviewmodel.Quiz.Questions;
+            //foreach (var question in quizviewmodel.Quiz.Questions)
+            //{
+            //    question.QuestionOptions = db.QuestionOption.Where(k => k.QuestionID == question.QuestionID).ToList();
+            //}
+            //quizviewmodel.Question = db.QuestionPool.Find(1);
+            //quizviewmodel.QuestionOptions = db.QuestionOptions.QuestionOptions.ToList();
+            return View("TakeQuiz",quizviewmodel);
+        }
+
+    }
+}
---
+++ AdaptiveLearningApplication/Models/FilteredQuestions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdaptiveLearningApplication.Models
+{
+    public class FilteredQuestions
+    {
+        public int QuizID { get; set; }
+        public string QuizName { get; set; }
+        public QuestionPoolModel Question { get; set; }
+        public int count { get; set; }
+        public int difficulty { get; set; }
+        public int correct { get; set; }
+        public List<int> listOfAnswers {get; set;}
+
+        public FilteredQuestions() 
+        {
+            listOfAnswers = new List<int>();
+            Question = new QuestionPoolModel();
+            count = 0;
+            difficulty = 0;
+        }
+    }
+}
